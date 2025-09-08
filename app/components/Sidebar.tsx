8751ab9a21e40767ca9e99aeb4efcc910d686efc
Serhii Safonov <137483700+KaratSergio@users.noreply.github.com>
2025-09-08 14:15:08 +0300

    icons

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -2,13 +2,17 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import LogoutButton from './auth/LogoutBtn';
+import {
+    Home, FileType, FileText,
+    Edit3, Settings
+} from 'lucide-react';
 
 const menu = [
-    { key: 'dashboard', label: 'Dashboard', href: '/page/dashboard' },
-    { key: 'templates', label: 'Templates', href: '/page/templates' },
-    { key: 'documents', label: 'Documents', href: '/page/documents' },
-    { key: 'signatures', label: 'Signatures', href: '/page/sign' },
-    { key: 'settings', label: 'Settings', href: '/page/settings' },
+    { key: 'dashboard', label: 'Dashboard', href: '/page/dashboard', icon: Home },
+    { key: 'templates', label: 'Templates', href: '/page/templates', icon: FileType },
+    { key: 'documents', label: 'Documents', href: '/page/documents', icon: FileText },
+    { key: 'signatures', label: 'Signatures', href: '/page/sign', icon: Edit3 },
+    { key: 'settings', label: 'Settings', href: '/page/settings', icon: Settings },
 ];
 
 export default function Sidebar() {
@@ -18,12 +22,14 @@ export default function Sidebar() {
         <aside className="w-40 bg-teal-900 text-white flex flex-col p-4">
             {menu.map(item => {
                 const isActive = pathname.startsWith(item.href);
+                const Icon = item.icon;
                 return (
                     <Link key={item.key} href={item.href}>
                         <button
-                            className={`mb-2 px-3 py-2 text-left rounded transition-colors duration-200
+                            className={`mb-2 px-3 py-2 text-left rounded flex items-center gap-2 transition-colors duration-200
                             ${isActive ? 'bg-teal-950' : 'hover:bg-teal-950'}`}
                         >
+                            <Icon className="w-5 h-5" />
                             {item.label}
                         </button>
                     </Link>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,13 @@
 import './shared/styles/globals.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'DocSP',
+  description: 'Example app',
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
