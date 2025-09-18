df1c7de6b9d9a0a724d4329bfe4ab7be80e0bbb2
Serhii Safonov <137483700+KaratSergio@users.noreply.github.com>
2025-09-18 15:29:39 +0300

    refactor upload route

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,22 +1,13 @@
-import { r2 } from '@/server/config/r2.config';
-import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { uploadFile } from "@/server/utils/s3Storage";
 
 export async function POST(req: Request) {
   const formData = await req.formData();
-  const file = formData.get('file') as File;
-  if (!file) return Response.json({ error: 'File required' }, { status: 400 });
+  const file = formData.get("file") as File;
+  if (!file) return Response.json({ error: "File required" }, { status: 400 });
 
   const buffer = Buffer.from(await file.arrayBuffer());
 
-  await r2.send(
-    new PutObjectCommand({
-      Bucket: process.env.S3_BUCKET!,
-      Key: file.name,
-      Body: buffer,
-    })
-  );
+  const fileUrl = await uploadFile(buffer, file.name, file.type);
 
-  // URL upload doc
-  const fileUrl = `https://${process.env.S3_BUCKET}.r2.cloudflarestorage.com/${file.name}`;
   return Response.json({ fileUrl });
 }
